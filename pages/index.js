a4cc01031df550cf26b3d90d3f14a9563a25aa49
John William Davis <jwdavisdeveloper@gmail.com>
2022-04-05 10:02:28 -0700

    Play with index page and drac components

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,22 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Box, Heading, Paragraph } from "@dracula/dracula-ui";
+import { Divider, Box, Heading, Paragraph, Text } from "@dracula/dracula-ui";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Next.js + TailwindCSS + Dracula UI</title>
+        <title>Morbius - A Fan Page - with DraculaUI</title>
       </Head>
-      <div className='grid place-items-center h-screen w-screen'>
-        <Box
-          color='yellowPink'
-          className='group text-center p-4 border rounded-xl shadow-sm shadow-cyan-300 hover:shadow-lg hover:shadow-cyan-300 transition-shadow'
-        >
-          <Image
-            src='/dracula.webp'
-            alt='dracula'
-            width='128'
-            height='128'
-            quality='90'
-            className='group-hover:scale-75 transition-transform'
-          />
-          <Heading color='black' weight='bold'>
-            Hello Vampires
-          </Heading>
-          <Paragraph color='black'>This should get you started with</Paragraph>
-          <Paragraph color='blackSecondary'>Next.js + TailwindCSS + Dracula UI.</Paragraph>
+      <div className='flex flex-col items-center h-screen w-screen bg-black'>
+        <div className='flex flex-col items-center mt-4 py-4'>
+          <Heading color='red'>Morbius</Heading>
+          <Box color='yellowPink' p='md' className='mt-6 rounded-2xl'>
+            <Text color='blackSecondary'>A new film from Marvel!</Text>
+          </Box>
+        </div>
+        <Box m='md' className='w-1/2'>
+          <Divider color='orange' />
         </Box>
       </div>
     </>
